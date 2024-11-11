7337bd373f0c69d17db9d61815216eddb556ad9d
Mathias <mathiasibehrens@gmail.com>
2024-11-11 23:31:40 +0100

    bestest_Run moved to constructor, working testlist

--- bestest.h
+++ bestest.h
@@ -9,10 +9,18 @@
 #include <setjmp.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #ifdef __cplusplus 
 extern "C" {
 #endif
+
+
+#if !defined(BESTEST_THREAD_LOCAL) && __STDC_VERSION__ >= 201112
+#include <threads.h>
+#define BESTEST_THREAD_LOCAL thread_local
+#endif
+
 
 #ifndef BESTEST_CODE_SECTION
 #define BESTEST_CODE_SECTION(s)  __attribute__((section(s)))
@@ -34,7 +42,7 @@ typedef struct bestest_Result {
     size_t numErrors;
 } bestest_Result;
 
-struct bestest_Test {
+struct bestest_Test  {
     const char *domain;
     const char *name;
     bestest_UnitTest test;
@@ -44,19 +52,19 @@ struct bestest_Test {
 
 struct bestest_Context {
     bestest_Result result;
-    struct bestest_Test *startOfTests;
-    struct bestest_Test *endOfTests;
+    const struct bestest_Test **startOfTests;
+    const struct bestest_Test **endOfTests;
     struct bestest_Options {
         int verbosity;
     } opts;
-    struct {
-        jmp_buf exitPoint;
-        const char *funcName;
-        const char *error;
-        size_t line;
-    } uut;
 };
 
+struct bestest_UnitUnderTest {
+    jmp_buf exitPoint;
+    const char *funcName;
+    const char *error;
+    size_t line;
+};
 
 /*
  * Assertions 
@@ -75,24 +83,26 @@ struct bestest_Context {
 /*
  * API
  */
-#define bestest_CreateGlobalContext()   \
-    extern struct bestest_Test __start_bestest_tests; \
-    extern struct bestest_Test __stop_bestest_tests; \
-    struct bestest_Context bestest_context = { .startOfTests = &__start_bestest_tests, .endOfTests = &__stop_bestest_tests }; 
+#define bestest_CreateGlobalContext(options)   \
+    extern const struct bestest_Test *__start_bestest_tests; \
+    extern const struct bestest_Test *__stop_bestest_tests; \
+    struct bestest_Context bestest_context = { .opts.verbosity = options, .startOfTests = &__start_bestest_tests, .endOfTests = &__stop_bestest_tests }; \
+    BESTEST_THREAD_LOCAL struct bestest_UnitUnderTest bestest_uut;
 
 
 #define BESTEST_TEST(domain, name) \
-    BESTEST_CODE_SECTION(".utest_functionpath") static const char* domain##_##name##path = #domain #name; \
+    BESTEST_CODE_SECTION(".utest_functionpath") static const char* domain##_##name##path = #domain "::" #name; \
     void domain##_##name(void *context); \
     BESTEST_WEAK extern bestest_Setup domain##_setupPtr; \
     BESTEST_WEAK extern bestest_TearDown domain##_teardownPtr; \
-    BESTEST_CODE_SECTION("bestest_tests") struct bestest_Test domain##_##name##Ptr = { #domain, #domain #name, &domain##_##name, &domain##_setupPtr, &domain##_teardownPtr }; \
+    const struct bestest_Test domain##_##name##_Meta = { #domain, #name, &domain##_##name, &domain##_setupPtr, &domain##_teardownPtr }; \
+    BESTEST_CODE_SECTION("bestest_tests") const struct bestest_Test *domain##_##name##_MetaPtr = &domain##_##name##_Meta; \
     void domain##_##name(void *context)
 
 #define BESTEST_SETUP(domain)       void *domain##_setup(void); bestest_Setup domain##_setupPtr = &domain##_setup; BESTEST_CODE_SECTION("bestest_setups")      void *domain##_setup(void)
 #define BESTEST_TEARDOWN(domain)    void domain##_teardown(void*); bestest_TearDown domain##_teardownPtr = &domain##_teardown; BESTEST_CODE_SECTION("bestest_teardowns")   void domain##_teardown(void *context)
 
-static inline bestest_Result bestest_Run(const char *testList[], const char *ignoreList[]);
+static inline bestest_Result bestest_Run(int argc, char *argv[]);
 
 static inline void bestest_SetVerbosity(int level);
 
@@ -110,20 +120,20 @@ unsigned long long bestest_Time(void);
  */
 
 extern struct bestest_Context bestest_context;
+extern thread_local struct bestest_UnitUnderTest bestest_uut;
 
-
-static inline void bestest_RunTest(struct bestest_Test *test)
+static inline void bestest_RunTest(const struct bestest_Test *test)
 {
 static char logMsg[BESTEST_ERROR_MESSAGE_MAX_LENGTH];
     unsigned long long endTime, startTime = bestest_Time();
     int offset = 0;
 
     void *testContext = test->setup ? (*test->setup)() : NULL;
-    if (setjmp(bestest_context.uut.exitPoint) == 0) {
+    if (setjmp(bestest_uut.exitPoint) == 0) {
         test->test(testContext);
         endTime = bestest_Time();
         if (bestest_context.opts.verbosity & BESTEST_LOG_SUCCESS) {
-            offset = snprintf(logMsg, sizeof(logMsg), "[SUCCESS] in %s::%s", test->domain, bestest_context.uut.funcName);
+            offset = snprintf(logMsg, sizeof(logMsg), "[SUCCESS] in %s::%s", test->domain, bestest_uut.funcName);
         }
         else
             logMsg[0] = 0;
@@ -131,7 +141,7 @@ static char logMsg[BESTEST_ERROR_MESSAGE_MAX_LENGTH];
     else {
         endTime = bestest_Time();
         bestest_context.result.numErrors++;
-        offset = snprintf(logMsg, sizeof(logMsg), "[ERROR  ] in %s::%s:%zu => %s", test->domain, bestest_context.uut.funcName, bestest_context.uut.line, bestest_context.uut.error);
+        offset = snprintf(logMsg, sizeof(logMsg), "[ERROR  ] in %s::%s:%zu => %s", test->domain, bestest_uut.funcName, bestest_uut.line, bestest_uut.error);
     }
     if (bestest_context.opts.verbosity & BESTEST_LOG_DURATION)
         snprintf(logMsg + offset, sizeof logMsg - offset, " (duration: %.3f)", ((double)endTime - (double)startTime) / 1000.0);
@@ -143,44 +153,69 @@ static char logMsg[BESTEST_ERROR_MESSAGE_MAX_LENGTH];
         bestest_LogResult(logMsg);
 }
 
-static inline bool bestest_TestInList(const char *list[], const char *test)
+static inline bool bestest_TestInList(const char *list, int len, const struct bestest_Test *test)
 {
-    for (const char **entry = list; *entry; entry++)
-        if (!strcmp(*entry, test)) 
+    const char *entry = list;
+    for (int i = 0; i < len; i++, entry += strlen(entry) + 1) {
+        static BESTEST_THREAD_LOCAL char fullName[BESTEST_ERROR_MESSAGE_MAX_LENGTH];
+        snprintf(fullName, sizeof fullName, "%s::%s", test->domain, test->name);
+        if (!strncmp(entry, fullName, strlen(entry)))
             return true;
+    }
     return false;
 }
 
-static inline bestest_Result bestest_Run(const char *testList[], const char *ignoreList[])
+__attribute__((constructor(0xFFFF)))
+static inline bestest_Result bestest_Run(int argc, char* argv[])
 {
-    for (struct bestest_Test *test = bestest_context.startOfTests; test < bestest_context.endOfTests; test++) {
-        if (ignoreList && bestest_TestInList(ignoreList, test->name))
+    struct {
+        char *list;
+        int len;
+    } testList = {}, ignoreList = {};
+    if (argc > 1) {
+        testList.list = argv[1];
+        testList.len = 1;
+        for (char* p = testList.list; *p; p++)
+            if (*p == ',')
+                *p = '\0', testList.len++;
+    }
+    if (argc > 2) {
+        ignoreList.list = argv[1];
+        ignoreList.len = 1;
+        for (char* p = ignoreList.list; *p; p++)
+            if (*p == ',')
+                *p = '\0', ignoreList.len++;
+    }
+    ptrdiff_t numTests = bestest_context.endOfTests - bestest_context.startOfTests;
+    for (const struct bestest_Test **test = bestest_context.startOfTests; test < bestest_context.endOfTests; test++) {
+        const struct bestest_Test *uut = *test;
+        if (ignoreList.list && bestest_TestInList(ignoreList.list, ignoreList.len, uut))
             continue;
-        if (!testList || bestest_TestInList(testList, test->name))
-            bestest_RunTest(test);
+        if (!testList.list || bestest_TestInList(testList.list, testList.len, uut))
+            bestest_RunTest(uut);
     }
 
-    return bestest_context.result;
+    exit(bestest_context.result.numErrors);
 }
 
 
 static inline void bestest_Assert_(bool expr, const char *funcName, size_t line, const char *exprText)
 {
-    bestest_context.uut.funcName = funcName;
+    bestest_uut.funcName = funcName;
     if (!expr) {
-        bestest_context.uut.error = exprText;
-        bestest_context.uut.line = line;
-        longjmp(bestest_context.uut.exitPoint, 1);
+        bestest_uut.error = exprText;
+        bestest_uut.line = line;
+        longjmp(bestest_uut.exitPoint, 1);
     }
 }
 
 static inline void bestest_AssertMemory_(void *p1, void *p2, size_t size, const char *funcName, size_t line, const char *exprText)
 {
-    bestest_context.uut.funcName = funcName;
+    bestest_uut.funcName = funcName;
     if (memcmp(p1, p2, size)) {
-        bestest_context.uut.error = exprText;
-        bestest_context.uut.line = line;
-        longjmp(bestest_context.uut.exitPoint, 1);
+        bestest_uut.error = exprText;
+        bestest_uut.line = line;
+        longjmp(bestest_uut.exitPoint, 1);
     }
 }
 
@@ -281,13 +316,13 @@ static inline void bestest_SetVerbosity(int level)
 #define BESTEST_ASSERTION(assertion, name, type, fmt) \
     static void bestest_Assert##assertion##name(type v1, type v2, const char *v1Name, const char *v2Name, size_t line, const char *funcName) \
     {                                                                                   \
-        bestest_context.uut.funcName = funcName;                                          \
+        bestest_uut.funcName = funcName;                                          \
         if (!(v1 BESTEST_ASSERTION_TYPE_##assertion v2)) {                                \
-            char msg[BESTEST_ERROR_MESSAGE_MAX_LENGTH];                                   \
+            static BESTEST_THREAD_LOCAL char msg[BESTEST_ERROR_MESSAGE_MAX_LENGTH];     \
             sprintf(msg, "%s == %s => " fmt " != " fmt, v1Name, v2Name, v1, v2);        \
-            bestest_context.uut.error = msg;                                              \
-            bestest_context.uut.line = line;                                              \
-            longjmp(bestest_context.uut.exitPoint, 1);                                    \
+            bestest_uut.error = msg;                                              \
+            bestest_uut.line = line;                                              \
+            longjmp(bestest_uut.exitPoint, 1);                                    \
         }                                                                               \
     }
 
